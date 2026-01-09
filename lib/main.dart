5173a29981253009528927e7786a5e7745ff0503
07rashmika <rashmikadesilva07@gmail.com>
2026-01-09 23:57:16 +0530

    loading screen added before going to the chat screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'package:chat_app/screens/chat.dart';
+import 'package:chat_app/screens/splash.dart';
 import 'package:chat_app/screens/auth.dart';
 
 Future<void> main() async {
@@ -25,7 +28,20 @@ class App extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 63, 17, 177),
         ),
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
